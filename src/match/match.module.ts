9a53b303278b1e65f6e2af60f704c0c96b1b47d9
Bruno Jimenez <omarjm0910@gmail.com>
2025-12-14 16:49:02 -0600

    corregimos importaciones

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -3,10 +3,12 @@ import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
 import { Match } from './entities/match.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Team } from 'src/team/entities/team.entity';
 
 @Module({
   controllers: [MatchController],
   providers: [MatchService],
-  imports: [TypeOrmModule.forFeature([Match])],
+  imports: [TypeOrmModule.forFeature([Match, Team])],
+  exports: [MatchService],
 })
 export class MatchModule {}
--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { Team } from './entities/team.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Match } from 'src/match/entities/match.entity';
 
 @Module({
   controllers: [TeamController],
   providers: [TeamService],
+  imports: [TypeOrmModule.forFeature([Team, Match])],
+  exports: [TeamService],
 })
 export class TeamModule {}
